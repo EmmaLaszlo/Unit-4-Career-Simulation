6d936a8e5cee4022e313de18e25bc6557dce2e9f
Emma Grace <emmalaslo@gmail.com>
2025-05-18 13:08:55 -0400

    updated middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -17,10 +17,10 @@ exports.protect = async (req, res, next) => {
     // verifies their JWT token
     // if the token is valid, it attaches the user’s data to the request so the route can use it
     const user = await db.query("SELECT * FROM users WHERE id = $1", [
-      decoded.id,
+      decoded.id, // decoded token includes the id number as well so we need to fetch that
     ]);
     req.user = user.rows[0];
-    next();
+    next(); // pass the request to the actual route handler
   } catch (err) {
     console.error("AUTH ERROR:", err.message);
     return res.status(401).json({ message: "Token failed" });
